0bf753343782c4eb42515055e47a57e724ca9a18
meena <meenavardhineedi25@gmail.com>
2026-03-29 04:36:53 -0700

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn complaint_portal.wsgi
--- complaint_project7/settings.py
+++ complaint_project7/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7m^vv+_zxkm%*r+05_wufb(a9=aa%3!54-oz+ll_f$cdonodq7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
