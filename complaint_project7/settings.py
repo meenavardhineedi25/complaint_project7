4d386f8309807573ff494463ddf2190e284d2127
meena <meenavardhineedi25@gmail.com>
2026-03-30 05:01:46 -0700

    Prepare for deployment: update settings for env vars and add STATIC_ROOT, add .gitignore

--- complaint_project7/settings.py
+++ complaint_project7/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7m^vv+_zxkm%*r+05_wufb(a9=aa%3!54-oz+ll_f$cdonodq7'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7m^vv+_zxkm%*r+05_wufb(a9=aa%3!54-oz+ll_f$cdonodq7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else ['*']
 
 
 # Application definition
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
